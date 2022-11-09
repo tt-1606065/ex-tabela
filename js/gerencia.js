06847e635540254243adb767bbdcbda8c2d09ead
SabinoWS <sabino.th@hotmail.com>
2022-11-08 21:05:59 -0300

    Adiciona inicio de correção

--- js/gerencia.js
+++ js/gerencia.js
@@ -1,3 +1,47 @@
 import "../scss/style.scss"
 
 console.log("JS DA GERENCIA")
+
+
+document.querySelector("#botaoCadastrar")
+    .addEventListener("click", executarCadastro)
+
+document.querySelector("#botaoDeletar")
+    .addEventListener("click", executarDelete)
+
+async function executarCadastro() {
+    // Pegando os valores dos inputs do HTML
+    const titulo = document.querySelector("#titulo").value
+    const descricao = document.querySelector("#descricao").value
+
+    // Mandar esses valores para a API guardar na Base de Dados
+    const result = await cadastrarCurso(titulo, descricao)
+    console.log(result)
+}
+
+
+async function cadastrarCurso(titulo, descricao) {
+    const corpoDaRequisicao = {
+        title: titulo,
+        description: descricao
+    }
+
+    const fetchResposta = await fetch("https://api-dwi.herokuapp.com/api/tutorials", {
+        method: "POST",
+        body: JSON.stringify(corpoDaRequisicao),
+        headers: { "Content-type": "application/json" }
+    })
+
+    const respostaEmJSON = await fetchResposta.json()
+    return respostaEmJSON
+}
+
+
+async function executarDelete(){
+    const fetchResposta = await fetch("https://api-dwi.herokuapp.com/api/tutorials", {
+        method: "DELETE"
+    })
+
+    const respostaEmJSON = await fetchResposta.json()
+    return respostaEmJSON
+}
--- js/index.js
+++ js/index.js
@@ -1,3 +1,48 @@
+
+
 import "../scss/style.scss"
 
 console.log("JS DA LISTA")
+
+executar()
+
+async function executar() {
+    
+    const cursosRetornados = await buscarValoresNaAPI()
+
+    // for (let index = 0; index < cursosRetornados.length; index++) {
+    //     console.log(" == VOLTA ==")
+    //     console.log(cursosRetornados[index].title)
+    //     console.log(cursosRetornados[index].description)
+    // }
+
+    let linhasTabela = `
+        <tr>
+            <td>Nome do Curso</td>
+            <td>Descrição do Curso</td>
+        </tr>
+    `
+    cursosRetornados.forEach(curso => {
+            linhasTabela += `
+            <tr>
+                <td>${ curso.title }</td>
+                <td>${ curso.description }</td>
+            </tr>
+        `
+    });
+
+    console.log(linhasTabela)
+
+    
+    // Alterando o valor da Tabela no HTML
+    // document.querySelector("#tabelaCurso").innerHTML = "VALOR FIXO NO CÓDIGO!"
+    document.querySelector("#tabelaCurso").innerHTML = linhasTabela
+}
+
+async function buscarValoresNaAPI(){
+    // const responseOriginal = await fetch("https://pokeapi.co/api/v2/pokemon/1")
+    const responseOriginal = await fetch("https://api-dwi.herokuapp.com/api/tutorials")
+    const response = await responseOriginal.json()
+
+    return response
+}
